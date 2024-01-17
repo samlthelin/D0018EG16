beca76358ad60028675698c0a6a95572d6de6de3
Albin Mårtensson <albin.martensson00@gmail.com>
2024-01-17 10:59:58 +0100

    Co-authored-by: Samuel Thelin <samthe-1@student.ltu.se> Co-authored-by: lelrek-1 <lelrek-1@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # D0018EG16
 cool databas??maybe?????
+Det är här vi borde skriva rapporten -->
+
--- apptest.py
+++ apptest.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
 
 app = Flask(__name__)
@@ -13,19 +13,27 @@ def create_connection():
     return mysql.connector.connect(**db_config)
 @app.route('/')
 
-def get_data():
-    try: 
-        connection = create_connection()
-        cursor = connection.cursor()
-        query = "SHOW DATABASES;"
-        cursor.execute(query)
-        data = cursor.fetchall()
-        cursor.close()
-        connection.close()
-        return render_template('index.html', data = data)
-    except Exception as e:
-        return f"An error occured: {str(e)}"
-    
+
+# def get_data():
+#     try: 
+#         connection = create_connection()
+#         cursor = connection.cursor()
+#         query = "SHOW TABLES;"
+#         cursor.execute(query)
+#         data = cursor.fetchall()
+#         cursor.close()
+#         connection.close()
+#         return render_template('index.html', data = data)
+#     except Exception as e:
+#         return f"An error occured: {str(e)}"
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+@app.route('/index2')
+def page2():
+    return render_template('index2.html')
 
 @app.route('/execute_function')
 def execute_function():
@@ -35,6 +43,17 @@ def execute_function():
 
     except Exception as e:
         return 
+
+@app.route('/execute_query', methods=['POST'])
+def execute_query():
+    query = request.form['query']
+    result = execute_sql_query(query)
+
+    return f"Query result: {result}"
+
+def execute_sql_query(query):
+    return f"Executing query: {query}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
